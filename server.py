06bc80153ce008fa9080fde0dcbec80e00bf0089
Norbert Schlaffke <norbert.schlaffke@student.put.poznan.pl>
2019-05-24 16:17:11 +0200

    Two way switching on server

--- server.py
+++ server.py
@@ -12,16 +12,17 @@ neighbors = UserSettableParameter('number', "Number of neighbors", 10, 1, 100, 1
 min_neighbors = UserSettableParameter('number', "Minimal number of neighbors", 5, 1, 100, 1)
 change_prob = UserSettableParameter('number', "Probability of random cooperation", 0.001, 0, 1)
 change_prob_dev = UserSettableParameter('number', "Standard deviation of PRC", 0.001, 0, 1)
-
+two_way = UserSettableParameter('checkbox', 'Two way switching', value=False)
 server = ModularServer(SocietyModel,
                        [chart],
-                       "Society model",
+                       "Cooperation emergence moodel - homogenous group",
                        {"N": n_slider,
                         'changeProb': change_prob,
                         'changeProbDev': change_prob_dev,
                         'isCoop': False,
                         'neighbors': neighbors,
-                        'minNeighbors': min_neighbors
+                        'minNeighbors': min_neighbors,
+                        'twoWay': two_way
                         })
 server.port = 8521 # The default
 server.launch()
